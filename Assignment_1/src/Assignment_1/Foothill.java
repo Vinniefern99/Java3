247113d4cdf1838035a6de7d7268fb0c94a601f6
vfernald <vfernald@SFDMDL0753C83C6.corp.intuit.net>
2015-04-11 19:15:45 -0700

    no notes

--- Assignment_1/src/Assignment_1/Foothill.java
+++ Assignment_1/src/Assignment_1/Foothill.java
@@ -12,41 +12,60 @@ public class Foothill
       ArrayList<Sublist> choices = new ArrayList<Sublist>();
       int k, j, numSets, max, kBest, masterSum;
       boolean foundPerfect;
-     
-      numSets = 0;
-      kBest = 0;
+
+      masterSum = 0;
 
       dataSet.add(2); dataSet.add(12); dataSet.add(22);
       dataSet.add(5); dataSet.add(15); dataSet.add(25);
       dataSet.add(9); dataSet.add(19); dataSet.add(29);
-      
+
       Sublist newSublist = new Sublist(dataSet);
       choices.add(newSublist);
+      numSets = 1;
+
+      //setting the first empty set to the best
+      kBest = 0;
+
+      //System.out.println(dataSet.size());
 
       //adds all possible sublists to the Array List choices.
       mainLoop:
-         for (k = 0 ; k < dataSet.size()-1 ; k++)
-            for (j = 0 ; j < choices.size()-1 ; j++)
+         for (k = 0 ; k < dataSet.size() ; k++)
+         {
+            ArrayList<Sublist> tempChoices = new ArrayList<Sublist>();
+            tempChoices = (ArrayList<Sublist>)choices.clone();
+            
+            for (j = 0 ; j < choices.size() ; j++)
             {
-               System.out.println("hi");
+               //System.out.println(k);
                int newPossibleSum = choices.get(j).getSum() + dataSet.get(k);
+               //System.out.println(newPossibleSum);
                if (newPossibleSum <= target)
                {
+                  //System.out.println("test");
                   choices.add(choices.get(j).addItem(dataSet.get(k)));
-                  numSets++;
-                  
-                  if (newPossibleSum > kBest)
-                     kBest = newPossibleSum;
                }
 
                if (newPossibleSum == target)
                {
-                  kBest = target;
+                  masterSum = target;
+                  foundPerfect = true;
                   break mainLoop;
                }
 
-            }
+               //System.out.println(choices.size());
 
-         choices.get(kBest).showSublist();
+            }
+         }
+
+
+      //System.out.println(choices.size());
+
+      
+
+      //choices.get(6).showSublist();
+
+      //System.out.println(kBest);
+      choices.get(kBest).showSublist();
    }
 }
